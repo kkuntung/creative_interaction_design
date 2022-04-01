0c2901ebd891cc32ecee40da3b2aaee6867d7724
kkuntung <100418773+kkuntung@users.noreply.github.com>
2022-04-01 21:14:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.write("hello world");
+
+function sayHello(){
+  var response = prompt("what is your name");
+  alert("hihihihi!"+response+",have a nice day!");
+}
+function add(a,b){
+  return a+b;
+}
+console.log(add(20,30));
+console.log(add(100,30));
+
+function showtopic(){
+  var x =document.getElementById('demo');
+  x.style.fontSize="25px";
+  x.style.color="blue";
+}
